bb0c090d19e9dbe77395747641b9b7d16cba34d1
Ric <ericschwartz7@gmail.com>
2024-04-09 23:40:57 -0400

    use assets:precompile

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-echo "building Tailwind CSS"
-bundle exec rake tailwindcss:build
+echo "compiling assets..."
+bundle exec rake assets:precompile
 
 echo "Puma phased restarting..."
 bundle exec pumactl phased-restart
