e4e38ac409842a2c31d5d813ec7d8b67492fd8fe
Ric <ericschwartz7@gmail.com>
2024-04-15 22:52:23 -0400

    need to set env to production when migrating

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,7 @@ echo "Installing gems..."
 bundle install
 
 echo "Database migrations..."
-bundle exec rake db:migrate
+bundle exec rake db:migrate RAILS_ENV=production
 
 echo "Compiling assets..."
 bundle exec rake assets:precompile
