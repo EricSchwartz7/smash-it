1831c9dbb13c2617656f3a633395434780a31b2d
Ric <ericschwartz7@gmail.com>
2024-04-19 15:18:08 -0400

    email address with name

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'info@ricschwartz.com'
+  default from: email_address_with_name('info@ricschwartz.com', 'Herb and Du')
 
   def cat_email
     @user = params[:user]
     @url  = 'https://ricschwartz.com'
-    mail(to: @user.email, subject: 'Herb and Du')
+    mail(to: @user.email, subject: 'Snackiessss')
   end
 end
